917779839d46a7afba77379fc21246fde82cd5ed
jovenden <jake@silverstripe.com>
2018-11-22 09:22:02 +1300

    Change default to php 7.1

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class ss_php::params(
-  $php_version = '5.6',
-  $cli_inifile = '/etc/php/5.6/cli/php.ini',
+  $php_version = '7.1',
+  $cli_inifile = '/etc/php/7.1/cli/php.ini',
   $dev = true,
   $apcu = true,
   $imagick = true,
