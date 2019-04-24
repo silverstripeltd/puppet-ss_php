36ad06c7377b9648c0d92fd56d831515d4a74b45
Indy Griffiths <indy@silverstripe.com>
2019-04-24 19:11:33 +1200

    Fix float conversion, remove dead code

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,20 +3,20 @@ class ss_php::install inherits ::ss_php {
     ensure => present,
   }
   alternatives { 'php':
-    path    => "/usr/bin/php${globals_php_version}",
+    path    => "/usr/bin/php${::ss_php::globals_php_version}",
     require => Ss_php::Package['cli'],
   }
 
-  if $dev {
+  if $::ss_php::dev {
     ss_php::package { 'dev':
       ensure => present,
     }
     alternatives { 'php-config':
-      path    => "/usr/bin/php-config${globals_php_version}",
+      path    => "/usr/bin/php-config${::ss_php::globals_php_version}",
       require => Ss_php::Package['dev'],
     }
     alternatives { 'phpize':
-      path    => "/usr/bin/phpize${globals_php_version}",
+      path    => "/usr/bin/phpize${::ss_php::globals_php_version}",
       require => Ss_php::Package['dev'],
     }
   } else {
--- spec/functions/ensure_php_packages_spec.rb
+++ spec/functions/ensure_php_packages_spec.rb
@@ -1,18 +1,18 @@
 require "spec_helper"
 
 describe "ensure_php_packages" do
-  it "creates a resource in the catalogue" do
-    is_expected.to run.with_params(["mbstring", "gd"], ["5.6", "7.1"])
-    expect(catalogue).to contain_package("php5.6-mbstring").with_ensure("present")
-    expect(catalogue).to contain_package("php7.1-mbstring").with_ensure("present")
-    expect(catalogue).to contain_package("php5.6-gd").with_ensure("present")
-    expect(catalogue).to contain_package("php7.1-gd").with_ensure("present")
-  end
-  it "can ensure absent resource in the catalog" do
-    is_expected.to run.with_params(["mbstring", "gd"], ["5.6", "7.1"], { "ensure" => "absent" })
-    expect(catalogue).to contain_package("php5.6-mbstring").with_ensure("absent")
-    expect(catalogue).to contain_package("php7.1-mbstring").with_ensure("absent")
-    expect(catalogue).to contain_package("php5.6-gd").with_ensure("absent")
-    expect(catalogue).to contain_package("php7.1-gd").with_ensure("absent")
-  end
+    it "creates a resource in the catalogue" do
+      is_expected.to run.with_params(["mbstring", "gd"], ["5.6", "7.1"])
+      expect(catalogue).to contain_package("php5.6-mbstring").with_ensure("present")
+      expect(catalogue).to contain_package("php7.1-mbstring").with_ensure("present")
+      expect(catalogue).to contain_package("php5.6-gd").with_ensure("present")
+      expect(catalogue).to contain_package("php7.1-gd").with_ensure("present")
+    end
+    it "can ensure absent resource in the catalog" do
+      is_expected.to run.with_params(["mbstring", "gd"], ["5.6", "7.1"], { "ensure" => "absent" })
+      expect(catalogue).to contain_package("php5.6-mbstring").with_ensure("absent")
+      expect(catalogue).to contain_package("php7.1-mbstring").with_ensure("absent")
+      expect(catalogue).to contain_package("php5.6-gd").with_ensure("absent")
+      expect(catalogue).to contain_package("php7.1-gd").with_ensure("absent")
+    end
 end
