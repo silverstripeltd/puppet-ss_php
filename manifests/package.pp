9163977096f24af0e520277280ca3869c83d133d
Indy Griffiths <indy@silverstripe.com>
2019-06-13 15:05:58 +1200

    Fix casting change

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,7 +2,8 @@ define ss_php::package(
   $ensure = 'present',
   $php_version = $::ss_php::globals_php_version
 ) {
-  if 0.0 + $php_version >= 7.2 and $name == 'mcrypt' {
+  $php_version_float = Numeric($php_version)
+  if $php_version_float >= 7.2 and $name == 'mcrypt' {
     notice('mcrypt is deprecated and unavailable for PHP >= 7.2. Skipping install')
   } else {
     package { "php${php_version}-${name}":
