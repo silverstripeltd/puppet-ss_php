229dc4e51a9b1d7588dfe0f668fe9e84d4700fc9
Indy Griffiths <indy@silverstripe.com>
2019-06-13 14:46:58 +1200

    validate_re only works for Strings

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,6 @@ class ss_php(
   $apcu = $::ss_php::params::apcu,
   $imagick = $::ss_php::params::imagick,
 ) inherits ::ss_php::params {
-  if $php_version != undef {
-    validate_re($php_version, '^[57].[0-9]')
-  }
-
   $globals_php_version = pick($php_version, $::ss_php::params::php_version)
   $globals_cli_inifile = "/etc/php/${globals_php_version}/cli/php.ini"
 
