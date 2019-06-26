41e2c2daa224ba631aa36a0fd76e678d63c523cb
Sean Harvey <sean@silverstripe.com>
2019-06-27 10:49:05 +1200

    Allow disabling managing cli package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 class ss_php(
   Optional[Pattern[/^[57].[0-9]/]] $php_version = $::ss_php::params::php_version,
+  $cli = $::ss_php::params::cli,
   $dev = $::ss_php::params::dev,
 ) inherits ::ss_php::params {
   $globals_php_version = pick($php_version, $::ss_php::params::php_version)
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,12 @@
 class ss_php::install inherits ::ss_php {
-  ss_php::package { 'cli':
-    ensure => present,
-  }
-  alternatives { 'php':
-    path    => "/usr/bin/php${::ss_php::globals_php_version}",
-    require => Ss_php::Package['cli'],
+  if $::ss_php::cli {
+    ss_php::package { 'cli':
+      ensure => present,
+    }
+    alternatives { 'php':
+      path    => "/usr/bin/php${::ss_php::globals_php_version}",
+      require => Ss_php::Package['cli'],
+    }
   }
 
   if $::ss_php::dev {
