4b373ecaac3c4f8d5a684b33254c1b55cd2540c0
John <john@silverstripe.com>
2024-07-15 12:17:19 +1200

    Allow custom repo config for Debian

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,24 +2,24 @@ class ss_php::repo {
   include apt
 
   apt::key { 'php':
-    id     => '15058500A0235D97F5D10063B188E2B695BD4743',
+    id      => '15058500A0235D97F5D10063B188E2B695BD4743',
     content => file('ss_php/sury-apt-php.gpg'),
   }
 
   ensure_packages(['apt-transport-https', 'lsb-release', 'ca-certificates'], {'ensure' => 'present'})
 
-  if ($::operatingsystem == 'Ubuntu') {
+  if ($os::name == 'Ubuntu') {
     apt::ppa { 'ppa:ondrej/php': }
-  } elsif $::operatingsystem == 'Debian' {
+  } elsif $os::name == 'Debian' {
     apt::source { 'php':
-      location    => 'https://packages.sury.org/php/',
-      release     => $::lsbdistcodename,
-      repos       => 'main',
-      notify      => Exec['ss_php_repo_force_update'],
-      include     => {
-        src => false
+      location => $ss_php::params::debian_repo_location,
+      release  => $os::distro::codename,
+      repos    => 'main',
+      notify   => Exec['ss_php_repo_force_update'],
+      include  => {
+        src => false,
       },
-      require     => [
+      require  => [
         Apt::Key['php'],
         Package['apt-transport-https', 'lsb-release', 'ca-certificates']
       ],
@@ -29,10 +29,9 @@ class ss_php::repo {
   }
 
   exec { 'ss_php_repo_force_update':
-      command     => "/bin/echo 'Apt update after adding sury repo'",
-      refreshonly => true,
-      logoutput   => true,
-      subscribe   => Exec['apt_update'],
+    command     => "/bin/echo 'Apt update after adding sury repo'",
+    refreshonly => true,
+    logoutput   => true,
+    subscribe   => Exec['apt_update'],
   }
-  
 }
